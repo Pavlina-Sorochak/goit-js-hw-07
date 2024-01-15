2899381bed42d4e6313e54a7b6c1c4ae26d8026a
Pavlina-Sorochak <143879103+Pavlina-Sorochak@users.noreply.github.com>
2024-01-15 14:18:40 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,6 +1,6 @@
 const button = document.querySelector(" button.change-color");
 const spanColor = document.querySelector(".color");
-const widget = document.querySelector(".widget");
+
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
